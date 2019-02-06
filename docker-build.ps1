3a72d6f58322052af69f3cd963233a182dc1761a
Craig Paton <craigpaton@gmail.com>
2019-02-06 20:46:55 +0000

    Use docker container to build wyam website

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,1 +1,27 @@
-docker build --tag capaton/notebook --file Dockerfile .
+[CmdletBinding()]
+param
+(
+    # Relative path to where output should be copied
+    [Parameter(Mandatory = $false)]
+    [string]
+    $OutputRelative = $("output")
+)
+
+$scriptFolder = $PSScriptRoot
+$outputFolder = Resolve-Path -Path ( Join-Path -Path $scriptFolder -ChildPath $OutputRelative )
+
+docker build --tag capaton/notebook --file Dockerfile-build .
+docker container create --name notebook-build capaton/notebook
+try
+{
+    if ( Test-Path -Path $outputFolder )
+    {
+        Remove-Item -Path $outputFolder -Recurse
+    }
+
+    docker container cp notebook-build:c:\app\output "$outputFolder"
+}
+finally
+{
+    docker container rm notebook-build
+}
